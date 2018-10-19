142d6bebc0a7bdd9551cbb23043adfdad8ef1d5a
tanxiazhe <tanxiazhe@163.com>
2018-10-19 16:26:46 +0800

    init springboot project

---
+++ src/main/java/com/maomao2/sweetalert/SweetAlertApplication.java
@@ -0,0 +1,12 @@
+package com.maomao2.sweetalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetAlertApplication.class, args);
+	}
+}
---
+++ src/test/java/com/maomao2/sweetalert/SweetAlertApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maomao2.sweetalert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SweetAlertApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
